2d72a9467670c0a59e4d249fbea7e181cbcfbcaf
VanLeDinh96 <dinhvanle.it@gmail.com>
2025-12-28 16:00:29 +0700

    +1 revise

---
+++ WildcardMatching/Practice/Greedy.cs
@@ -0,0 +1,39 @@
+﻿namespace WildcardMatching;
+
+public sealed class Greedy
+{
+    public static bool IsMatch(string s, string p)
+    {
+        int i = 0, j = 0;
+        int asteriskIndex = -1;
+        int matchIndex = 0;
+        while (i < s.Length)
+        {
+            if (j < p.Length && (s[i] == p[j] || p[j] == '?'))
+            {
+                i++;
+                j++;
+            }
+            else if (j < p.Length && p[j] == '*')
+            {
+                asteriskIndex = j;
+                j++;
+            }
+            else if (asteriskIndex != -1)
+            {
+                j = asteriskIndex + 1;
+                matchIndex++;
+                i = matchIndex;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        while(j < p.Length && p[j] == '*')
+        {
+            j++;
+        }
+        return j == p.Length;
+    }
+}
--- WildcardMatching/Program.cs
+++ WildcardMatching/Program.cs
@@ -3,30 +3,30 @@
 // Example test cases
 var testCases = new[]
 {
-    //new { s = "aa", p = "a", expected = false },
-    //new { s = "aa", p = "*", expected = true },
-    //new { s = "cb", p = "?a", expected = false },
-    //new { s = "adceb", p = "*a*b", expected = true },
-    //new { s = "acdcb", p = "a*c?b", expected = false },
-    //new { s = "", p = "", expected = true },
-    //new { s = "", p = "*", expected = true },
-    //new { s = "a", p = "", expected = false },
-    //new { s = "abc", p = "???", expected = true },
-    //new { s = "abc", p = "??", expected = false },
-    //new { s = "abc", p = "*?", expected = true },
-    //new { s = "abc", p = "?*", expected = true },
-    //new { s = "abc", p = "*a*", expected = true },
-    //new { s = "abc", p = "*b*", expected = true },
-    //new { s = "abc", p = "*d*", expected = false },
-    //new { s = "mississippi", p = "m*si*pi", expected = true },
-    //new { s = "mississippi", p = "m*si*pp", expected = false },
-    //new { s = "aab", p = "c*a*b", expected = false },
-    //new { s = "aab", p = "*a*b", expected = true },
+    new { s = "aa", p = "a", expected = false },
+    new { s = "aa", p = "*", expected = true },
+    new { s = "cb", p = "?a", expected = false },
+    new { s = "adceb", p = "*a*b", expected = true },
+    new { s = "acdcb", p = "a*c?b", expected = false },
+    new { s = "", p = "", expected = true },
+    new { s = "", p = "*", expected = true },
+    new { s = "a", p = "", expected = false },
+    new { s = "abc", p = "???", expected = true },
+    new { s = "abc", p = "??", expected = false },
+    new { s = "abc", p = "*?", expected = true },
+    new { s = "abc", p = "?*", expected = true },
+    new { s = "abc", p = "*a*", expected = true },
+    new { s = "abc", p = "*b*", expected = true },
+    new { s = "abc", p = "*d*", expected = false },
+    new { s = "mississippi", p = "m*si*pi", expected = true },
+    new { s = "mississippi", p = "m*si*pp", expected = false },
+    new { s = "aab", p = "c*a*b", expected = false },
+    new { s = "aab", p = "*a*b", expected = true },
     new { s = "abcdef", p = "a*e", expected = false }
 };
 
 foreach (var test in testCases)
 {
-    bool result = WildcardMatcherGreedy.IsMatch(test.s, test.p);
+    bool result = Greedy.IsMatch(test.s, test.p);
     Console.WriteLine($"IsMatch(\"{test.s}\", \"{test.p}\") = {result} (Expected: {test.expected})");
 }
