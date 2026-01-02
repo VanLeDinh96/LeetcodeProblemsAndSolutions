a848983c03d50f38fbdab5be18469655355d89fe
VanLeDinh96 <dinhvanle.it@gmail.com>
2026-01-02 23:57:26 +0700

    update practice of histogram

---
+++ LargestRectangleArea/Program.cs
@@ -0,0 +1,15 @@
+﻿using LargestRectangleArea.Solutions;
+
+//Test([2, 4, 6, 8]);
+Test([2, 1, 5, 6, 2, 3]);
+//Test([3, 3, 3, 3]);
+//Test([1, 3, 2, 1, 2]);
+//Test([5]);
+//Test([6, 5, 4, 3, 2, 1]);
+static void Test(int[] histogram)
+{
+    Console.WriteLine("Histogram: [" + string.Join(", ", histogram) + "]");
+    int result = HistogramMonotonicStackSolver.LargestRectangleArea(histogram);
+    Console.WriteLine("Max Rectangle Area = " + result);
+    Console.WriteLine(new string('-', 40));
+}
---
+++ LargestRectangleArea/Solutions/HistogramMonotonicStackSolver.cs
@@ -0,0 +1,36 @@
+﻿namespace LargestRectangleArea.Solutions;
+
+public sealed class HistogramMonotonicStackSolver
+{
+    public static int LargestRectangleArea(int[] heights)
+    {
+        Stack<int> stack = new();
+        stack.Push(-1);
+        int maxArea = 0;
+
+        for (int i = 0; i < heights.Length; i++)
+        {
+            while (stack.Peek() != -1 && heights[i] < heights[stack.Peek()])
+            {
+                int h = heights[stack.Pop()];
+                int width = i - stack.Peek() - 1;
+                int area = h * width;
+                maxArea = Math.Max(maxArea, area);
+            }
+
+            stack.Push(i);
+        }
+
+        int n = heights.Length;
+
+        while (stack.Peek() != -1)
+        {
+            int h = heights[stack.Pop()];
+            int width = n - stack.Peek() - 1;
+            int area = h * width;
+            maxArea = Math.Max(maxArea, area);
+        }
+
+        return maxArea;
+    }
+}
